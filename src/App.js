7df727f1a8f022e64f92b726440b4d2f55405c8b
PRATIKSHAGODBOLE <126340923+PRATIKSHAGODBOLE@users.noreply.github.com>
2023-10-11 18:35:41 +0530

    Games updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import Movies from './Components/Movies'
+// import Movie_Card from './Components/Movie_card'
+// export default function App() {
+//   // console.log(Movies)
+//   return (
+//     <div className="container" style={{ marginLeft: "10px" }}>
+//       <h1 className="text-center text-danger my-5 ml-10">Movie Cards</h1>
+//       {/* create card in row vice */}
+//       <div className="row">
+//         {
+//           // Movies.js se data App.js me liya using (Movies.map)
+//           Movies.map((movie, i) => (
+//             // col-md-4 columne device 4
+//             <div className="col-md-4 mb-4" >
+//               <Movie_Card movi={movie} key={i} />
+//             </div>
+//           ))
+//         }
+//       </div>
+//     </div>
+//   )
 
-function App() {
+// }
+
+import React from 'react'
+import TicTacToe from './Games/TicTacToe/TicTacToe'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+  <TicTacToe/>
 
-export default App;
+    </div>
+  )
+}
